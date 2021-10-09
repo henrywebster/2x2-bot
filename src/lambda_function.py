b1b8829df69d845f7afb733b59a6d9b2164ded88
Henry Webster <hwebs@hwebs.info>
2021-10-09 11:32:52 -0400

    Add s3 integration

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,12 +3,11 @@ import boto3
 import tweepy
 import random
 
+from fs import open_fs
 
-def select_item(table):
-    items = table.scan()["Items"]
-    # TODO check if there are no unposted tweets
-    # Can I shut off the trigger?
-    return random.choice([i for i in items if not i["posted"]])
+
+def create_response(statusCode, message):
+    return {"statusCode": statusCode, "body": {"message": message}}
 
 
 def update_posted(table, id):
@@ -27,15 +26,31 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(os.getenv("DB_TABLE_NAME"))
 
-    item = select_item(table)
+    items = table.scan()["Items"]
+    if not items:
+        return create_response(400, "Database is empty")
 
-    auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET"))
-    auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
+    unposted_items = [i for i in items if not i["posted"]]
+    if not unposted_items:
+        return create_response(400, "Database has no unposted content")
 
-    api = tweepy.API(auth)
+    item = random.choice(unposted_items)
+    filename = item["object"]
 
-    api.update_status(item["text"])
+    s3fs = open_fs("s3://{}/".format(os.getenv("S3_BUCKET_NAME")))
+    with s3fs.open(filename, "rb") as f:
+
+        auth = tweepy.OAuthHandler(
+            os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET")
+        )
+        auth.set_access_token(
+            os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET")
+        )
+        api = tweepy.API(auth)
+
+        media = api.simple_upload(filename, file=f)
+        api.update_status(item["text"], media_ids=[media.media_id])
 
     update_posted(table, item["id"])
 
-    return {"statusCode": 200}
+    return create_response(200, "Posted to Twitter")
